7346adf16a7c75ce3ea291557986fd0d7456819a
z.hoseini <z.hoseini@asiatech.ir>
2021-12-28 13:50:51 +0330

    added header section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.components';
@@ -8,6 +9,8 @@ import ShopPage from './pages/shop/shop.components';
 function App() {
   return (
     <div className="App">
+      <Header />
+
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
@@ -17,4 +20,3 @@ function App() {
 }
 
 export default App;
- 
